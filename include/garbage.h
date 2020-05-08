71e4c0fb696567524ec4cdf0e7b75a95beefab8b
Jason Nguyen <jnguyen1098@gmail.com>
2020-05-08 09:46:48 -0400

    fixed some stuff

--- include/garbage.h
+++ include/garbage.h
@@ -1,10 +1,40 @@
-int t, r;
 void *p, *s[100000];
+size_t t;
+
 static inline void f() {
-  for (int i = 0; i < t; i++)
-    s[i] = p && s[i] == p ? (p = NULL, NULL) : s[i], free(p ? 0x0 : s[i]), s[i] = p ? s[i] : 0;
+
+#if 1
+    for (size_t i = 0; i < t; i++)
+        s[i] = p && s[i] == p ? (p = NULL, NULL) : (free(s[i]), NULL);
+#endif
+
+#if 0
+    for (size_t i = 0; i < t; i++) {
+        if (p && s[i] == p)
+            s[i] = p = NULL;
+        else
+            s[i] = (free(s[i]), NULL);
+    }
+#endif
+
+
+#if 0
+    for (unsigned i = 0; i < t; i++) {
+        if (p) {
+            if (s[i] == p) {
+                s[i] = NULL;
+                p = NULL;
+            }
+        }
+        else {
+            free(s[i]);
+            s[i] = NULL;
+        }
+    }
+#endif
 }
+
 #define free(x)
-#define malloc(x)(calloc(1, x))
+#define malloc(x)(s[t++]=malloc(x))
 #define calloc(x,y)(s[t++]=calloc(x, y))
 #define realloc(x,y)((p = x, f()),s[t++]=realloc(x,y))
