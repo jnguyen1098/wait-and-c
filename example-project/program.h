dfd94ab994002217cf699a3d62e88738a0fa2d59
Jason Nguyen <jnguyen1098@gmail.com>
2020-05-14 06:04:58 -0400

    removed segfault detector...unreliable

--- example-project/program.h
+++ example-project/program.h
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#include "../garbage.h"           // normal garbage collector
-//#include "../garbage_seg.h"       // garbage.h but with segfault support
+#include "../garbage.h"           // normal garbage collector
 //#include "../garbage_fast.h"      // garbage.h butwithout atexit()
-#include "../garbage_expanded.h"  // garbage.h but expanded with comments
+//#include "../garbage_expanded.h"  // garbage.h but expanded with comments
 
 char *this_malloc_comes_from_the_far_lands(void);
