65a1a051a15360c1b2654acaa4a5607e63ff1ec7
Jason Nguyen <jnguyen1098@gmail.com>
2020-05-14 00:08:30 -0400

    simplified structure

--- example-project/program.h
+++ example-project/program.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "garbage.h"
+
+#include "garbage_aio.h" // garbage collector setup
 
 char *this_malloc_comes_from_the_far_lands(void);
