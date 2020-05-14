ffe61cec6e532e9c1c34d034bb1dcce38653055d
Jason Nguyen <jnguyen1098@gmail.com>
2020-05-14 00:37:51 -0400

    simpler naming

--- example-project/program.h
+++ example-project/program.h
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "garbage_aio.h" // garbage collector setup
+#include "garbage.h" // garbage collector setup
 
 char *this_malloc_comes_from_the_far_lands(void);
